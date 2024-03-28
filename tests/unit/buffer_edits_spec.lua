f26627afa6564b84f7f5268aade7a479880d9b3e
Jan Steinke <jan.steinke@gmail.com>
2024-03-28 02:25:54 +0100

    cleanup tests a bit

--- tests/unit/buffer_edits_spec.lua
+++ tests/unit/buffer_edits_spec.lua
@@ -1,18 +1,22 @@
-local buffer_edits = require("lsp-preview.buffer_edits")
 local describe = require("plenary.busted").describe
+local it = require("plenary.busted").it
 local after_each = require("plenary.busted").after_each
 local before_each = require("plenary.busted").before_each
 local assert = require("luassert.assert")
 
+local buffer_edits = require("lsp-preview.buffer_edits")
 
 describe("the buffer editor", function()
 	local temp_buf
+
 	before_each(function()
 		temp_buf = vim.api.nvim_create_buf(false, true)
 	end)
+
 	after_each(function()
 		vim.api.nvim_buf_delete(temp_buf, { force = true })
 	end)
+
 	it("caluclates the diff correctly", function()
 		vim.api.nvim_buf_set_lines(temp_buf, 0, -1, false, { "bar" })
 
--- tests/unit/config_spec.lua
+++ tests/unit/config_spec.lua
@@ -1,13 +1,20 @@
+local describe = require("plenary.busted").describe
+local it = require("plenary.busted").it
+local before_each = require("plenary.busted").before_each
+local assert = require("luassert.assert")
+
 local config = require("lsp-preview.config")
 
 describe("the config", function()
 	before_each(function()
 		config.__reset()
 	end)
+
 	it("uses the default values", function()
 		config.setup({})
 		assert.equals(false, config.preview)
 	end)
+
 	it("sets the user config", function()
 		config.setup({ preview = true })
 		assert.equals(true, config.preview)
